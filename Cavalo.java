82242757312d7a444ce9ce108959e03c3a0d600d
Barbarakmenezes <barbarakerolaine@hotmail.com>
2022-07-17 23:10:10 -0300

    classeabstrata

--- Cavalo.java
+++ Cavalo.java
@@ -1,10 +1,26 @@
-package orientacaobjeto;
+package exercicios;
 
-public class Cavalo {
-        public class cavalo extends Animal {
-        	
-        	public void devecorrer() {
-        		System.out.println("Ele está correndo...");
-        	}
-        }
+public class Cavalo extends Animal{
+	
+
+	public Cavalo() {
+		super("Cachorro");
+	}
+	
+	@Override
+	public void Nome (String nomeanimal) {
+		System.out.println("Nome do cavalo: "+nomeanimal);
+	}
+	@Override
+	public void Idade (int idadeanimal) {
+		System.out.println("Idade: "+idadeanimal);
+	}
+	@Override
+	public void Som (String somanimal) {
+		System.out.println("Som que o seu cavalo emite: "+somanimal);
+	}
+	public void Corre () {
+		System.out.println("Uma das caracteristicas do cavalo é correr!");
+	}
+	
 }
